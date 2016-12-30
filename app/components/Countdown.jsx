5f0b1421abe39c7314720e2bb726d296daabf1ff
Michael Simmelbauer <mas@Michaels-MacBook-Pro.local>
2016-12-30 12:27:01 +0100

    style clock

--- app/components/Countdown.jsx
+++ app/components/Countdown.jsx
@@ -1,10 +1,12 @@
 var React = require('react');
 
+var Clock = require('Clock');
+
 var Countdown = React.createClass({
   render: function() {
     return(
       <div>
-        <p>Countdown.jsx</p>
+        <Clock totalSeconds={129} />
       </div>
     );
   }
